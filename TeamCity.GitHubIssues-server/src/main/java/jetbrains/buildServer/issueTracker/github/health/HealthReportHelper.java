892699ab6284fb36f0e87f88103b2b67c0cd9529
Oleg Rybak <oleg.rybak@jetbrains.com>
2020-11-20 15:06:15 +0100

    TW-68870 Add allowNetworkCall wrapper to repository issues check

--- TeamCity.GitHubIssues-server/src/main/java/jetbrains/buildServer/issueTracker/github/health/HealthReportHelper.java
+++ TeamCity.GitHubIssues-server/src/main/java/jetbrains/buildServer/issueTracker/github/health/HealthReportHelper.java
@@ -1,7 +1,8 @@
 package jetbrains.buildServer.issueTracker.github.health;
 
-import java.io.IOException;
+import jetbrains.buildServer.serverSide.IOGuard;
 import jetbrains.buildServer.serverSide.oauth.github.GitHubClientSSL;
+import jetbrains.buildServer.util.FuncThrow;
 import org.eclipse.egit.github.core.Repository;
 import org.eclipse.egit.github.core.service.RepositoryService;
 import org.jetbrains.annotations.NotNull;
@@ -10,9 +11,11 @@ public class HealthReportHelper {
 
   public boolean hasIssues(@NotNull final String owner, @NotNull final String repo) {
     try {
-      final Repository repository = new RepositoryService(new GitHubClientSSL()).getRepository(owner, repo);
-      return repository.isHasIssues();
-    } catch (IOException ignored) {
+      return IOGuard.allowNetworkCall((FuncThrow<Boolean, Exception>)() -> {
+        final Repository repository = new RepositoryService(new GitHubClientSSL()).getRepository(owner, repo);
+        return repository.isHasIssues();
+      });
+    } catch (Exception ignored) {
     }
     return false;
   }
